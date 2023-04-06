a565a58ca9b457d15d5f582de36b7efb8ade3158
preethamkumar343 <113992331+preethamkumar343@users.noreply.github.com>
2023-04-06 17:34:02 +0530

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.jpg";
+import product3 from "./assets/products/3.jpg";
+import product4 from "./assets/products/4.jpg";
+import product5 from "./assets/products/5.jpg";
+import product6 from "./assets/products/6.jpg";
+import product7 from "./assets/products/7.jpg";
+import product8 from "./assets/products/8.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "T shirt",
+    price: 599,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Puma runner v1 red",
+    price: 3999,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Quartz Analog clock",
+    price: 999,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Canva shoe",
+    price: 1299,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Adidas Furz shoe",
+    price: 4499,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Sprectrum Analog watch",
+    price: 1799,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Puma Shoe",
+    price: 3899,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Polex Watch 2",
+    price: 2249,
+    productImage: product8,
+  },
+];
